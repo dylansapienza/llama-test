c0613a0b65c9f3eaf3d84b571759b6c7adc107cc
Dylan Sapienza <dylsapienza12@gmail.com>
2025-03-21 16:56:02 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -225,3 +225,9 @@ const DinnerInvitation = () => {
 };
 
 export default DinnerInvitation;
+
+export default function Home() {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
+  );
+}
